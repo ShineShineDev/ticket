4f27a7465658b644aa0b1237709da7f155985e88
Spidey Shine <49630548+ShineShineDev@users.noreply.github.com>
2024-02-11 11:36:54 +0630

    Update config.js

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://ec2-3-27-191-151.ap-southeast-2.compute.amazonaws.com';
